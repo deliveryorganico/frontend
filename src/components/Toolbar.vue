78cf5e19e02c18e347b583f648efc2056ce47ce7
GinoAntonel <ginoantonel.ga@gmail.com>
2018-11-14 23:06:14 -0300

    Redireccionamiento login y register toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,48 +1,48 @@
 <template>
-  <div id="Toolbar">
+  <v-toolbar class="amber">
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link</v-btn>
+      <v-btn flat href="/login">Login</v-btn>
+      <v-btn flat href="/register">Register</v-btn>
+    </v-toolbar-items>
     <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-      >
-        <v-list class="pa-1">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg">
-            </v-list-tile-avatar>
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
 
-            <v-list-tile-content>
-              <v-list-tile-title>John Leider</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
 
-        <v-list class="pt-0" dense>
-          <v-divider></v-divider>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
 
-          <v-list-tile
-            v-for="item in items"
-            :key="item.title"
-          >
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
 
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-    <v-toolbar class="amber">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Delivery Organico</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  </div>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-toolbar>
 </template>
 
 <script>
