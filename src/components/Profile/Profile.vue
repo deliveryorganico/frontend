2e130b7b6ede94e34d45b9265f8bb76f4fd46eac
Benjamin Gilardini <benjamin.gilardini@gmail.com>
2018-11-16 12:06:30 -0300

    Avance en el Profile

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -1,7 +1,75 @@
 <template>
   <div id="profile">
     <Toolbar></Toolbar>
-    <h1>Profile</h1>
+    <v-layout column justify-center>
+
+    <v-expansion-panel popout>
+      <v-expansion-panel-content
+        v-for="(message, i) in messages"
+        :key="i"
+        hide-actions
+      >
+        <v-layout
+          slot="header"
+          align-center
+          row
+          spacer
+        >
+          <v-flex xs4 sm2 md1>
+            <v-avatar
+              slot="activator"
+              size="36px"
+            >
+              <v-icon
+                v-if="message.avatar"
+                alt="Avatar"> account_circle
+              </v-icon>
+
+            </v-avatar>
+          </v-flex>
+
+          <v-flex sm5 md3 hidden-xs-only>
+            <strong v-html="message.name"></strong>
+            <span
+              v-if="message.total"
+              class="grey--text"
+            >
+              &nbsp;({{ message.total }})
+            </span>
+          </v-flex>
+
+          <v-flex no-wrap xs5 sm3>
+            <v-chip
+              v-if="message.new"
+              :color="`${message.color} lighten-4`"
+              class="ml-0"
+              label
+              small
+            >
+              {{ message.new }} new
+            </v-chip>
+            <strong v-html="message.title"></strong>
+          </v-flex>
+
+          <v-flex
+            v-if="message.excerpt"
+            class="grey--text"
+            ellipsis
+            hidden-sm-and-down
+          >
+            &mdash;
+            {{ message.excerpt }}
+          </v-flex>
+        </v-layout>
+
+        <v-card>
+          <v-divider></v-divider>
+          <v-card-text v-text="lorem">
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-layout>
     <Pie></Pie>
   </div>
 </template>
@@ -15,6 +83,16 @@
     components: {
       Pie,
       Toolbar
-    }
-  }
+    },
+    data: () => ({
+      messages: [
+        {
+          avatar: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+          name: 'User Name',
+          title: 'Descripcion Personal',
+
+        }
+      ],
+    })
+}
 </script>
