f28276395e0de05061b13ff18df4aac1ca7910a9
JuanDubowez <juancruzdubowez@gmail.com>
2018-11-30 01:48:09 -0300

    Search

--- src/components/Carrito/Carrito.vue
+++ src/components/Carrito/Carrito.vue
@@ -8,40 +8,27 @@
           <v-toolbar-title>Carrito</v-toolbar-title>
 
           <v-spacer></v-spacer>
-
-          <v-btn icon>
-            <v-icon>search</v-icon>
-          </v-btn>
+          <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>  
         </v-toolbar>
-
+        
         <v-list three-line>
           <template v-for="(item, index) in items">
-            <v-subheader
-              v-if="item.header"
-              :key="item.header"
-            >
+            <v-subheader v-if="item.header" :key="item.header">
               {{ item.header }}
             </v-subheader>
 
             <v-divider
-              v-else-if="item.divider"
-              :inset="item.inset"
-              :key="index"
-            ></v-divider>
+              v-else-if="item.divider" :inset="item.inset" :key="index">
+            </v-divider>
 
-            <v-list-tile
-              v-else
-              :key="item.title"
-              avatar
-              @click=""
-            >
+            <v-list-tile v-else :key="item.title" avatar @click="" :search="search">
               <v-list-tile-avatar>
                 <img :src="item.avatar">
               </v-list-tile-avatar>
 
               <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                <v-list-tile-title> {{ item.title }} </v-list-tile-title>
+                <v-list-tile-sub-title> {{ item.subtitle }} </v-list-tile-sub-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-btn color="red" dark>
@@ -60,18 +47,19 @@
   export default {
     data () {
       return {
+        search: '',
         items: [
           { header: 'Productos' },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
-            title: 'Cafe Badilico',
-            subtitle: "<span class='text--primary'>Precio</span> &mdash; $192"
+            title: 'Fruta',
+            subtitle: "$192"
           },
           { divider: true, inset: true },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
             title: 'Cafe Badilico',
-            subtitle: "<span class='text--primary'>Precio</span> &mdash; $192"
+            subtitle: "$192"
           },
         ]
       }
