77a197b2fbfd0193ff022c520cc3c17ae58628fd
Ivan Nunez <ivanluis.cba@gmail.com>
2018-12-02 23:32:20 -0300

    login con vuetify arreglado

--- src/components/User/Login.vue
+++ src/components/User/Login.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="form">
-    <form action="#" @submit.prevent="login">
-      <label for="username">Username:</label>
-      <input id="username" name="username" v-model="username" type="text">
-      <label for="password">Password:</label>
-      <input id="password" name="password" v-model="password" type="password">
-      <button type="submit">Login</button>
-    </form>
+    <v-app>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+              <v-card class="elevation-12">
+                <v-toolbar color="amber">
+                  <v-toolbar-title>Login Form</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form @submit.prevent="login">
+                    <v-text-field v-model="username" prepend-icon="person" name="username" label="Username" type="text"/>
+                    <v-text-field v-model="password" prepend-icon="lock" name="password" label="Password" type="password"/>
+                    <v-card-actions>
+                      <v-spacer/>
+                      <v-btn color="primary" type="submit">Login</v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
